e9229c9b499f24eeccfaec948eadf93583fe18e9
sebinbejoy <sebinbejoy1304@gmail.com>
2024-11-15 11:18:45 +0530

    Implemented Authentication using jwt

---
+++ src/main/java/com/sebin/employee_poc/repository/UserInfoRepository.java
@@ -0,0 +1,12 @@
+package com.sebin.employee_poc.repository;
+
+import com.sebin.employee_poc.entity.UserInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserInfoRepository extends JpaRepository<UserInfo, Integer> {
+    Optional<UserInfo> findByEmail(String email); // Use 'email' if that is the correct field for login
+}
